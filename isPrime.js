d7e3ba74daf5dea03a539a6cd0d0c3b325febf9d
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-03-01 09:29:55 +0530

    feat: Checking prime number completed

---
+++ isPrime.js
@@ -0,0 +1,16 @@
+// n = 5
+function isPrime(n){
+    if ( n < 2) {
+        return false;
+    }
+    for (let i = 2; i < n; i++) {
+        if ( n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPrime(1)) // false
+console.log(isPrime(2)) // true
+console.log(isPrime(6)) // false
